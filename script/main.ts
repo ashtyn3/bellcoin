a48933d7920526cf8da6b87b178b12c57b2e50f9
ashtyn3 <ashtyn@gitmono.com>
2021-01-20 17:26:17 -0500

    finished parser

--- script/main.ts
+++ script/main.ts
@@ -4,7 +4,7 @@ const lines: Array<string> = text.split("\n");
 interface statement {
   Type?: string;
   name: string;
-  value: string | Array<string>;
+  value: Array<statement | string>;
 }
 let AST: Array<statement> = [];
 let mode = "normal";
@@ -16,21 +16,41 @@ lines.forEach((l, n, a) => {
       AST.push({
         Type: "RET",
         name: lexems[2],
-        value: lexems[3],
+        value: [lexems[3]],
       });
     }
-    console.log(AST);
-    if (AST[AST.length - 1]?.value == "") {
+    if (AST[AST.length - 1]?.value[0] == "") {
       mode = "def";
     }
+  }
+  if (l.trim() == "END") {
+    mode = "normal";
   }
   if (l.match(/([A-Z]+\s)?(\w+)\s([\s\S]+),/)) {
     const lexems: Array<string> = l
       .trim()
       .split(/([A-Z]+\s)?(\w+)\s([\s\S]+),/);
     if (mode == "def") {
+      if (typeof AST[AST.length - 1].value == "string") {
+        AST[AST.length - 1].value = [
+          {
+            Type: lexems[1],
+            name: lexems[2],
+            value: [lexems[3]],
+          },
+        ];
+      } else {
+        AST[AST.length - 1].value.push({
+          Type: lexems[1],
+          name: lexems[2],
+          value: [lexems[3]],
+        });
+      }
+    } else {
       AST.push({
         Type: lexems[1],
+        name: lexems[2],
+        value: [lexems[3]],
       });
     }
   }
